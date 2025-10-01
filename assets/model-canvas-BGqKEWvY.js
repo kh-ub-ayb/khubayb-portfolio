bc9368fb08e21f7ce4f6e30538c1ed5888b93806
kh-ub-ayb <khubayb05@gmail.com>
2025-10-01 18:43:02 +0530

    Updates

--- assets/model-canvas-BGqKEWvY.js
+++ assets/model-canvas-BGqKEWvY.js
@@ -1,4 +1,4 @@
-import{r as ae,a as yM,g as xM,j as Xt,c as AM}from"./index-CKAWfcZV.js";/**
+import{r as ae,a as yM,g as xM,j as Xt,c as AM}from"./index-C5ZgRqn1.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
